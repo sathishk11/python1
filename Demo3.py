f4d97adf4bb1500703133d677b5c67a3a8ee73df
Admin <skksathish258@gmail.com>
2022-12-21 15:16:41 +0530

    second

---
+++ Demo3.py
@@ -0,0 +1,19 @@
+a=3
+print(a ** 3)
+#presidence of operators
+print(5+8-2)
+print(5-8+2)
+#String manipulation
+str1="welcome"
+print(len(str1))
+print(str1[2])
+a=1234
+c=str(a)
+print(type(c))
+#operations in string #find
+print(str1.find("l"))
+print(str1.find("a"))
+print(str1.upper())
+print(str1)
+print(str1.count("e")) #count
+print(str1.isupper())
