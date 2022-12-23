682479f5f0a7bc54cccd98d91d5411f1be2017c0
Admin <skksathish258@gmail.com>
2022-12-23 16:33:37 +0530

    fourth

--- tuples.py
+++ tuples.py
@@ -1,4 +1,4 @@
-#methods is tuples, tupple is declared in a () brackets
+#methods in tuples, tupple is declared in a () brackets
 #count
 l=(12,43,78,12,34,54) #tuple declaration
 print(l.count(12))
@@ -11,3 +11,6 @@ z=l+a
 print(z)
 print(a*3) # it will print 3 times of "a" value
 print(a[2]) # it will print the index value of a
+print("*")
+print("*","*")
+print("*","*","*")
